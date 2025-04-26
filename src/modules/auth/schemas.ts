6004f4d0aa131a0ff8aa443d6c75878befdc41fb
Randriatseheno <rajostelly@gmail.com>
2025-04-26 10:58:36 +0200

    FIX : Error message related to username

--- src/modules/auth/schemas.ts
+++ src/modules/auth/schemas.ts
@@ -5,7 +5,7 @@ export const registerSchema = z.object({
   password: z.string().min(6),
   username: z
     .string()
-    .min(4, "Username must be at least 3 characters long")
+    .min(4, "Username must be at least 4 characters long")
     .max(63, "Username must be at most 63 characters long")
     .regex(
       /^[a-z0-9][a-z0-9-]*[a-z0-9]$/,
