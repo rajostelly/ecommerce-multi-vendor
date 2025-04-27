01dbda384d621fffd35606cbb5a7c2c5e76a87e7
Randriatseheno <rajostelly@gmail.com>
2025-04-27 14:35:05 +0200

    FEAT: Add auth states

--- src/app/(app)/(auth)/sign-in/page.tsx
+++ src/app/(app)/(auth)/sign-in/page.tsx
@@ -1,6 +1,12 @@
+import { caller } from "@/trpc/server";
+import { redirect } from "next/navigation";
+
 import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
 
-const SignInPage = () => {
+const SignInPage = async () => {
+  const session = await caller.auth.session();
+
+  if (session.user) redirect("/");
   return <SignInView />;
 };
 
